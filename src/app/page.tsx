d25b57d711f56a87cc7d0526040d1cdf254dff06
Aymen tigui <aymentigui@gmail.com>
2024-12-27 16:13:37 +0100

    page home update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -358,6 +358,91 @@ export default function Home() {
             </p>
           </div>
         </section>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8 p-8'>
+            <motion.div
+              className="mt-16 bg-gradient-to-r from-blue-50 to-blue-100 p-8 rounded-lg shadow-lg"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+            >
+              <h2 className="text-3xl font-extrabold text-blue-800 mb-6 text-center">Nos Mission</h2>
+              <p className="mb-2 leading-8">
+                Le Groupement Formactive Center poursuit une mission claire
+              </p>
+              <ul className="space-y-2">
+                {[
+                  'Inspirer le développement en transformant la formation continue en levier',
+                  'permanent d’apprentissage et de progrès individuel et collectif ',
+                  'Renforcer les compétences individuellesOffrir aux apprenants les connaissances et les outils nécessaires pour exceller dans leur carrière et s\'adapter aux défis d\’un monde en constante évolution.',
+                  'Accompagner les entreprises dans leur croissance Offrir un accompagnement complet et stratégique pour aider les entreprises à développer leur capital humain et à atteindre leurs objectifs de performance.',
+                ].map((mission, index) => (
+                  <motion.li
+                    key={index}
+                    className="flex items-center space-x-4"
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.4 + index * 0.2, duration: 0.4 }}
+                  >
+                    <span className="w-3 h-3 bg-blue-500 rounded-full flex-shrink-0"></span>
+                    <span className="text-sm font-medium text-gray-700">{mission}</span>
+                  </motion.li>
+                ))}
+              </ul>
+            </motion.div>
+
+            <motion.div
+              className="mt-16 bg-gradient-to-r from-blue-50 to-blue-100 p-8 rounded-lg shadow-lg"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+            >
+              <h2 className="text-3xl font-extrabold text-blue-800 mb-6 text-center">Nos Valeurs</h2>
+              <ul className="space-y-2">
+                {[
+                  'Ouverture sur le monde professionnel',
+                  'Accessibilité : La formation doit être accessible à tous et à toutes. C\’est pourquoi nous nous efforçons de proposer des programmes flexibles et inclusifs.',
+                  'Adaptabilité : Anticiper et répondre aux défis émergents dans un monde en constante évolution.',
+                  'Innovation : Développer des approches et des outils novateurs pour offrir des solutions de formation adaptées aux besoins en évolution des individus et des entreprises.',
+                  'Éthique et transparence : Nous opérons avec intégrité et transparence, en privilégiant des relations de confiance avec tous nos interlocuteurs.'
+                ].map((valeur, index) => (
+                  <motion.li
+                    key={index}
+                    className="flex items-center space-x-2"
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ delay: 0.4 + index * 0.2, duration: 0.4 }}
+                  >
+                    <span className="w-3 h-3 bg-blue-500 rounded-full flex-shrink-0"></span>
+                    <span className="text-sm font-medium text-gray-700">{valeur}</span>
+                  </motion.li>
+                ))}
+              </ul>
+            </motion.div>
+          </div>
+          <motion.div
+            className="mt-8 bg-gradient-to-r from-blue-50 to-blue-100 mx-8 p-8 rounded-lg shadow-lg"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+            <h2 className="text-3xl font-extrabold text-blue-800 mb-6 text-center">Notre Vision</h2>
+            <ul className="space-y-4">
+              {[
+                'Le Grp Formactive Center aspire à transformer le paysage de la formation continue en répondant directement aux besoins en formation et inspirer la culture d\’apprentissage tout au long de la vie.',
+              ].map((mission, index) => (
+                <motion.li
+                  key={index}
+                  className="flex items-center space-x-4"
+                  initial={{ opacity: 0, x: -20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.4 + index * 0.2, duration: 0.4 }}
+                >
+                  <span className="w-3 h-3 bg-blue-500 rounded-full flex-shrink-0"></span>
+                  <span className="text-sm font-medium text-gray-700">{mission}</span>
+                </motion.li>
+              ))}
+            </ul>
+          </motion.div>
 
         {/* Contact Form Section */}
         <section className="py-16">
