6eb000163e49ef19dde963f3b376162688491680
Aymen tigui <aymentigui@gmail.com>
2024-12-28 12:24:12 +0100

    test Image

--- src/app/(public)/formations-images/[id]/page.tsx
+++ src/app/(public)/formations-images/[id]/page.tsx
@@ -59,7 +59,7 @@ export default function FormationPage() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-        >
+        > 
           <Card className="overflow-hidden">
             <CardHeader className="relative p-0">
               {formation.photo && (
